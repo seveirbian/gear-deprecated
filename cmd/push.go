ea607dcef2c82f2015b85d6d32cd578a940147d6
Bian Shengwei <bianshengwei@gmail.com>
2019-01-07 09:02:21 +0800

    initially finish seaweed_setup.py

---
+++ cmd/push.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+    "fmt"
+    "github.com/spf13/cobra"
+    "github.com/seveirbian/gear/push"
+    "github.com/seveirbian/gear/pkg/image"
+    // "github.com/seveirbian/gear/pkg/gear"
+)
+
+var dockerImage string
+
+func init() {
+    rootCmd.AddCommand(buildCmd)
+    buildCmd.Flags().StringVarP(&dockerImage, "docker-image", "", "", "build a gear image from a standard docker image")
+}   
+
+var buildCmd = &cobra.Command{
+    Use:   "build",
+    Short: "build a gear image from a standard docker image",
+    Long:  `build a gear image from a standard docker image`,
+    Run: func(cmd *cobra.Command, args []string) {
+        image := image.Parse(dockerImage)
+
+        gearImageBuilder := build.InitBuilder(image)
+
+        if gearImageBuilder.HasParsedThisImage() {
+            fmt.Println("This image has been built.")
+        }else {
+            gearImageBuilder.Build()
+        }
+    },
+}
---
+++ others/seaweed_setup.py
@@ -0,0 +1,34 @@
+import sys
+# package need to be installed, pip install docker
+import docker
+import time
+# package need to be installed, pip install pyyaml
+import yaml
+import os
+
+class Runner:
+
+    def __init__(self, image, ports, command, remove, waitline):
+        self.image = image
+        self.ports = ports
+        self.command = command
+        self.remove = remove
+        self.waitline = waitline
+
+
+    def start(self):
+        image_pulled = client.images.pull(self.image)
+
+        container = client.containers.create(image=self.image, ports=self.ports,
+                                        command=self.command, detach=True, remove=self.remove)
+
+        container.start()
+
+        while True:
+            if container.logs().find(self.waitline) >= 0:
+                break
+
+
+if __name__ == "__main__":
+    runner := Runner(image="chrislusf/seaweedfs:latest",
+        ports={"9333/tcp":"9333"}, command="server", remove=true, waitline="added volume server")
